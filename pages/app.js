df723bc9a2b1aea52d6fd58f2ce678573fdb60a9
mdarsalan-c30 <105696006+mdarsalan-c30@users.noreply.github.com>
2024-07-31 02:41:01 +0530

    Initial commit

---
+++ pages/app.js
@@ -0,0 +1,35 @@
+// Function to load HTML content into specified element
+function loadHTML(url, elementId) {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(elementId).innerHTML = data;
+            if (elementId === 'header') {
+                setupMenuToggle(); // Ensure toggle function is initialized after header is loaded
+            }
+        })
+        .catch(error => console.error('Error loading HTML:', error));
+}
+
+// Initialize header and footer once the DOM is loaded
+document.addEventListener('DOMContentLoaded', () => {
+    loadHTML('/pages/header.html', 'header');
+    loadHTML('/pages/footer.html', 'footer');
+});
+
+// Setup mobile menu toggle functionality
+function setupMenuToggle() {
+    const menuToggle = document.getElementById('mobile-menu');
+    const navMenu = document.querySelector('.nav-menu');
+
+    menuToggle.addEventListener('click', () => {
+        navMenu.classList.toggle('active');
+    });
+}
+
+// Load Tools
+fetch('./pages/tools.html') // Adjust path as needed
+.then(response => response.text())
+.then(data => {
+    document.getElementById('tool').innerHTML = data;
+});
